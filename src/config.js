38ddc44eb9a9809fc348394da637d14e2eb0a9fd
Nuwan Upekshana <nuwan.upekshana@gmail.com>
2022-01-31 01:46:55 +1100

    Init productO App

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  google: {
+    API_KEY: "",
+    CLIENT_ID: "",
+    SECRET: "",
+  },
+  facebook: {
+    APP_ID: "",
+  },
+  app :{
+    SIGNUP_ENABLED : false,
+    SMLOGIN_ENABLED : false
+  }
+}
